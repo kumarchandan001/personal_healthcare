9eb21513616465292f4dc6a448503ff6465a4e1d
chandankumar0528 <chandankumar0528@github.com>
2025-07-01 21:03:52 +0530

    add

--- app.py
+++ app.py
@@ -864,4 +864,6 @@ def submit_prescription():
 if __name__ == "__main__":
     check_navigation_routes()
     initialize_app()
-    app.run(debug=True, port=5000) 
+    # app.run(debug=True, port=5000) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
